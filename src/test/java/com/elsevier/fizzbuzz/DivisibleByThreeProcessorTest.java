5bf6ae088c64ba63cadc0c344ac3bc9938c271e3
cryerd <d.cryer@elsevier.com>
2018-12-07 12:25:43 +0000

    move divisible by three tests to own file

--- src/test/java/com/elsevier/fizzbuzz/DivisibleByThreeProcessorTest.java
+++ src/test/java/com/elsevier/fizzbuzz/DivisibleByThreeProcessorTest.java
@@ -1,4 +1,28 @@
 package com.elsevier.fizzbuzz;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class DivisibleByThreeProcessorTest {
+
+    @Test
+    public void substituteNumberThree() {
+        DivisibleByThreeProcessor divisibleByThreeProcessor= new DivisibleByThreeProcessor();
+        CompositeObject request = new CompositeObject(3, "");
+        CompositeObject result = divisibleByThreeProcessor.process(request);
+
+        assertEquals(result.getIndex(), 3);
+        assertEquals(result.getValue(), "Fizz");
+    }
+
+    @Test
+    public void doNotSubstituteNumberNotDivisibleByThree() {
+        DivisibleByThreeProcessor divisibleByThreeProcessor= new DivisibleByThreeProcessor();
+        CompositeObject request = new CompositeObject(2, "");
+        CompositeObject result = divisibleByThreeProcessor.process(request);
+
+        assertEquals(result.getIndex(), 2);
+        assertEquals(result.getValue(), "");
+    }
 }
