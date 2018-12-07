bb811013a4d6a206aec4b35d24ca685f4d832651
cryerd <d.cryer@elsevier.com>
2018-12-07 12:18:13 +0000

    add fizz buzz class to get sequence and print to console

---
+++ src/main/java/com/elsevier/fizzbuzz/FizzBuzzConsole.java
@@ -0,0 +1,8 @@
+package com.elsevier.fizzbuzz;
+
+import java.util.List;
+
+public interface FizzBuzzConsole {
+
+    void print(List<String> sequence);
+}
--- src/test/java/com/elsevier/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/com/elsevier/fizzbuzz/FizzBuzzTest.java
@@ -2,10 +2,22 @@ package com.elsevier.fizzbuzz;
 
 import org.junit.Test;
 
+import java.util.List;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 public class FizzBuzzTest {
 
     @Test
-    public void dummy() {
-
+    public void run() {
+        FizzBuzzConsole output = mock(FizzBuzzConsole.class);
+        //noinspection unchecked
+        List<String> sequence = mock(List.class);
+        SequenceGenerator sequenceGenerator = mock(SequenceGenerator.class);
+        when(sequenceGenerator.generate(1, 10)).thenReturn(sequence);
+        new FizzBuzz(sequenceGenerator, output).run(1, 10);
+        verify(output).print(sequence);
     }
 }
